44919d39c9c0beef4f3b6c6d9f597fd878ae6c24
fatih-berktug <faatihberktug@hotmail.com>
2021-03-12 00:55:18 +0300

    search güncelleme

--- sbs/Forms/GtasinmazSearchForm.py
+++ sbs/Forms/GtasinmazSearchForm.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 
 from sbs.models.Gtasinmaz import Gtasinmaz
@@ -6,20 +7,11 @@ from sbs.models.Gtasinmaz import Gtasinmaz
 class GtasinmazSearchForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(GtasinmazSearchForm, self).__init__(*args, **kwargs)
-        # self.fields['name'].required = False
-        # self.fields['definition'].required = False
-        # self.fields['UsageArea'].required = False
-        # self.fields['block'].required = False
-        # self.fields['floor'].required = False
-        # self.fields['tkgmno'].required = False
-        # self.fields['sirano'].required = False
-        # self.fields['tasinmazinTuru'].required = False
-        #
-        # self.fields['mulkiyet'].required = False
-        # self.fields['arsaDegeri'].required = False
-        # self.fields['yapiRaic'].required = False
-        # self.fields['yapiMalitet'].required = False
+        self.fields['name'].required = False
 
+        self.fields['tkgmno'].required = False
+        self.fields['sirano'].required = False
+        self.fields['tasinmazinTuru'].required = False
 
     class Meta:
         model = Gtasinmaz
@@ -27,53 +19,24 @@ class GtasinmazSearchForm(ModelForm):
         fields = (
             'name', 'sirano', 'tkgmno', 'tasinmazinTuru',)
 
-        # labels = {'name': 'Tanımı',
-        #           'sirano': 'Sıra numarası',
-        #           'block': 'Blok Adeti',
-        #           'floor': 'Kat Sayısı',
-        #           'mulkiyet': 'Mülkiyet',
-        #           'tkgmno': 'Tkgm numarası',
-        #           'UsageArea': 'Kullanılan Alan (m2) ',
-        #           'tahsis_durumu': "Tahsis durumu",
-        #           'tasinmazinTuru': 'Taşınmazın Türü',
-        #           'definition': 'Açıklama',
-        #           'arsaDegeri': 'Arsa Degeri:',
-        #           'yapiMalitet': 'Yapı Maliyet degeri ',
-        #           'yapiRaic': 'Yapı Raiç Degeri',
-        #           'arsaTuru': 'Bina mı?'
-        #
-        #           }
+        labels = {'name': 'Tanımı',
+                  'sirano': 'Sıra numarası',
 
-        # widgets = {
-        #     'yapiMalitet': forms.TextInput(
-        #         attrs={'class': 'form-control ', }),
-        #     'yapiRaic': forms.TextInput(
-        #         attrs={'class': 'form-control ', }),
-        #     'arsaDegeri': forms.TextInput(
-        #         attrs={'class': 'form-control ', }),
-        #
-        #     'arsaTuru': forms.Select(attrs={'class': 'form-control select2 select2-hidden-accessible',
-        #                                     'style': 'width: 100%; '}),
-        #
-        #     'mulkiyet': forms.Select(attrs={'class': 'form-control select2 select2-hidden-accessible',
-        #                                     'style': 'width: 100%; ', }),
-        #     'tahsis_durumu': forms.Select(attrs={'class': 'form-control select2 select2-hidden-accessible',
-        #                                          'style': 'width: 100%; ', }),
-        #     'tasinmazinTuru': forms.Select(attrs={'class': 'form-control select2 select2-hidden-accessible',
-        #                                           'style': 'width: 100%; '}),
-        #     'sirano': forms.TextInput(
-        #         attrs={'class': 'form-control '}),
-        #     'tkgmno': forms.TextInput(
-        #         attrs={'class': 'form-control '}),
-        #     'floor': forms.TextInput(
-        #         attrs={'class': 'form-control ', }),
-        #     'block': forms.TextInput(
-        #         attrs={'class': 'form-control ', }),
-        #     'UsageArea': forms.TextInput(
-        #         attrs={'class': 'form-control ', }),
-        #     'name': forms.TextInput(
-        #         attrs={'class': 'form-control ', }),
-        #     'definition': forms.Textarea(
-        #         attrs={'class': 'form-control ', 'rows': '2'}),
-        #
-        # }
+                  'tkgmno': 'Tkgm numarası',
+                  'tasinmazinTuru': 'Tasinmazin Türü'
+
+                  }
+
+        widgets = {
+            'yapiMalitet': forms.TextInput(
+                attrs={'class': 'form-control ', }),
+
+            'sirano': forms.TextInput(
+                attrs={'class': 'form-control '}),
+            'tkgmno': forms.TextInput(
+                attrs={'class': 'form-control '}),
+
+            'name': forms.TextInput(
+                attrs={'class': 'form-control ', }),
+
+        }
